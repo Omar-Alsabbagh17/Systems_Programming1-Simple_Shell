d855327ca7a5073e8c69a809e56b01e19b786586
Omar Alsabbagh <42855230+Omar-Alsabbagh17@users.noreply.github.com>
2022-10-12 09:21:07 -0700

    Update sshell.c
    > fixed the error  where nothing was showing to the terminal if command was supplied without arguments
    > added input redirection feature
    > fixed the issue where the string was overwriting the other string after copying
    > simplified some codes

--- sshell.c
+++ sshell.c
@@ -100,7 +100,7 @@ int main(void)
                 else{
                         string_vector v;
                         vec_init(&v);
-                        char cmd_copy[strlen(cmd)+1];
+                        char* cmd_copy = (char*) malloc(strlen(cmd)+1);
                         strcpy(cmd_copy, cmd);
                         char failed = cmd_parser(cmd_copy, &v);
                         if (failed)
@@ -108,49 +108,70 @@ int main(void)
                         char is_program = 1; // to distinguish program from argument
                         char is_out_redirection = 0;
                         char is_pip = 0;
+                        char finished_input_redirection = 0;
                         unsigned int count = 0; // to keep track of arguments
                         program prog;
+                        
                         for (int i = 0; i < v.total; i++)
                         {
                                 if (count >= ARGS_MAX)
                                 {
                                         fprintf(stderr, "ERROR: too many arguments\n");
                                         break;
-
                                 }
                                 if (is_program)
                                 {
-                                        // check if it's a valid command
-                                        char temp_cmd[strlen(v.items[i])+30];
-                                        strcpy( temp_cmd, v.items[i]);
-                                        strcat(temp_cmd, " > /dev/null 2>&1");
-                                        int ret = system(temp_cmd);
-                                        if (ret != 0)
-                                        {
-                                                printf("ERROR: command not found\n");
-                                                break;
-                                        }
-
+                                       
                                         count = 0;
                                         prog.command = v.items[i];
                                         prog.args[count++]=v.items[i];
+
+                                        /* if we reach end of cmd, then it means
+                                         it's command without any arguments */
+                                        if (i == (v.total-1))
+                                        {
+                                                retval = execute(&prog, count);
+                                                fprintf(stderr, "+ completed '%s' [%d]\n", cmd, retval);
+
+                                        }
                                         is_program = 0; 
                                 }
                                 else if (!strcmp(v.items[i], ">"))
                                 {
                                         is_out_redirection = 1;
                                         continue;
+                                }
+                                
+                                else if (!strcmp(v.items[i], "<"))
+                                {
+                                        finished_input_redirection = 1;
+                                        prog.args[count++]=v.items[i+1];
+                                        retval = execute(&prog, count);
+                                        
+                                        fprintf(stderr, "+ completed '%s' [%d]\n", cmd, retval);
+                                        continue;
 
+                                }
+                                else if (finished_input_redirection)
+                                {
+                                        finished_input_redirection = 0;
+                                        is_program = 1;
+                                        continue;
                                 }
                                 else if (!strcmp(v.items[i], "|"))
                                 {
+                                        
+                                        // excute
+                                        // store exit status in an array
+                                        // redirect 
                                         is_pip = 1;
                                         is_program = 1;
+                                        prog.command = NULL;
                                         continue;
 
                                 }
                                 else if (i == (v.total-1)) // reached end of cmd
-                                {
+                                {                                        
                                         if (is_out_redirection)
                                         {       int fd = open(v.items[i], O_RDWR);
                                                 int screen_stdout = dup(STDOUT_FILENO);
@@ -163,6 +184,7 @@ int main(void)
                                                 fprintf(stderr, "+ completed '%s' [%d]\n", cmd, retval);
                                                 
                                         }
+                                        
                                         else
                                         {
                                                 prog.args[count++]=v.items[i];
@@ -193,14 +215,45 @@ int main(void)
 char cmd_parser(char* cmd, string_vector* v)
 {
         char * token = strtok(cmd, " ");
+        char found_sub_token = 0;
         
         while (token != NULL)
         {
-                //printf("%s\n", token);
-                vec_add(v, token);
+                // check if we have > or < but without whitespace
+                if (strlen(token) > 1)
+                        for (unsigned int i = 0; i < strlen(token); ++i)
+                        {
+                                if (token[i] == '>' || token[i] == '<')
+                                {
+                                        char delimeter[2] = {token[i], '\0'}; //since strtok doesn't accpet char
+                                        found_sub_token = 1;
+                                        char* token_copy;
+                                        token_copy = (char*) malloc(strlen(token)+1);
+                                        //memcpy(token_copy, token, strlen(token));
+                                        strcpy(token_copy, token);
+                                        char * sub_token = strtok(token_copy, delimeter);
+                                        if (i > 0) // it means there was no whitespace before and after
+                                        { 
+                                                vec_add(v, sub_token);
+                                                vec_add(v,delimeter);
+                                                sub_token = strtok(NULL, delimeter);
+                                                vec_add(v, sub_token);
+                                        }
+                                        else // it means there was no whitespace only after
+                                        {
+                                                vec_add(v, delimeter);
+                                                vec_add(v, sub_token);
+
+                                        }
+                                }
+                        }
+                
+                if (!found_sub_token)
+                        vec_add(v, token);
                 token = strtok(NULL, " ");
+                found_sub_token = 0;
         }
-        if (!strcmp(v->items[0], "|") || !strcmp(v->items[0], ">"))
+        if (!strcmp(v->items[0], "|") || !strcmp(v->items[0], ">") || !strcmp(v->items[0], "<"))
         {
                 fprintf(stderr, "ERROR: missing command\n");
                 return FAILED; 
@@ -215,8 +268,14 @@ char cmd_parser(char* cmd, string_vector* v)
                 fprintf(stderr, "ERROR: no output file\n");
                 return FAILED; 
         }
+        if (!strcmp(v->items[v->total-1], "<"))
+        {
+                fprintf(stderr, "ERROR: no input file\n");
+                return FAILED; 
+        }
         for (int i = 0; i < v->total; i++)
         {
+                //printf("%s\n", v->items[i]);
                 if (!strcmp(v->items[i], ">"))
                 {
                         for (int j = i+1; j < v->total; j++)
@@ -227,7 +286,7 @@ char cmd_parser(char* cmd, string_vector* v)
                                         return FAILED;
                                 }
                         }
-                        int fd = open(v->items[i+1] , O_WRONLY | O_CREAT, 0777);
+                        int fd = open(v->items[i+1] , O_WRONLY | O_CREAT | O_TRUNC, 0777);
                         if (fd == -1)
                         {
                                 fprintf(stderr,"ERROR: cannot open output file\n");
@@ -236,7 +295,22 @@ char cmd_parser(char* cmd, string_vector* v)
                         }
                         close(fd);
                 }
+                if (!strcmp(v->items[i], "<"))
+                {
+                       
+                        int fd = open(v->items[i+1] , O_RDONLY);
+                        
+                        if (fd == -1)
+                        {
+                                fprintf(stderr,"ERROR: cannot open input file\n");
+                                close(fd);
+                                return FAILED;
+                        }
+                        close(fd);
+                }
         }
+        // for (int i = 0; i < v->total; i++)
+        //         printf("%s\n", v->items[i]);
         return 0;
 }
 
@@ -244,24 +318,31 @@ char cmd_parser(char* cmd, string_vector* v)
 int execute(program * prog, unsigned int num_args)
 {
         char* args_list[num_args+1];
-        for (int i = 0; i < num_args; i++)
+        for (unsigned int i = 0; i < num_args; i++)
         {
                 args_list[i] = prog->args[i];
-                //printf("%s\n", args_list[i]);
+                //printf("%s\n", prog->args[i]);
         }
         args_list[num_args]= NULL;
         pid_t pid;
         pid = fork();
 
+
         if (pid == 0) {
                 // child
                 int out = execvp(prog->command,  args_list); // if fails, returns -1
+                if (out == -1)
+                {
+                        printf("ERROR: command not found\n");
+                        return 1;
+                }
+                
         }
-        else if (pid > 0)
+        else if (pid != 0)
         {
                 //parent
                 int status;
-                waitpid(pid, &status, 0); // wait for child to finish
+                wait(&status); // wait for child to finish
                 // fprintf(stderr, "+ completed '%s' [%d]\n", prog->command, WEXITSTATUS(status));
                 return WEXITSTATUS(status);
 
@@ -272,4 +353,5 @@ int execute(program * prog, unsigned int num_args)
                 //free(v->items);
                 exit(1);
         }
+        return 0;
 }
